100e9d06964e3c08aa5c3dae1aaac24e630ac6b8
Matt Hickey <mjh060@mun.ca>
2019-06-22 17:45:58 -0230

    Initial Commit

---
+++ SampleVulkan/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SampleVulkan
+//
+//  Created by Matthew Hickey on 2019-06-22.
+//  Copyright © 2019 matthewhickey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
